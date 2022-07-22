b6c7671eb7623eb17cd1d7596ff4cd5d3a61d3bc
Billy <billybdev@gmail.com>
2022-07-22 15:54:42 -0700

    solves problem 31

--- 031.js
+++ 031.js
@@ -1,21 +1,20 @@
+console.log(poundPermutations(200));
 
+function poundPermutations(n){
+    const coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    const table = Array(n + 1).fill(0);
+    table[0] = 1;
 
+    // seed table with each coin
+    // diagram in images folder
+    for (let coin of coins){
+        for (let i = 1; i <= n; i++){
+            const prev = i - coin;
+            if (prev < 0) continue;
+            table[i] += table[prev];
+        }
+    }
 
+    return table[n];
 
-
-
-
-
-
-
-
-
-// In the United Kingdom the currency is made up of pound (£) and pence (p). There are eight coins in general circulation:
-
-// 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
-
-// It is possible to make £2 in the following way:
-
-// 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
-
-// How many different ways can £2 be made using any number of coins?
+}
